ce5002b6e4f16e5273e80df34870a30af88fbc97
jiarenlu <gunbanmi@gmail.com>
2017-09-14 13:53:05 +0800

    fix build bug

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -2,8 +2,7 @@ import { Component,Input } from '@angular/core';
 import { Hero } from './hero';
 @Component({
     selector: "hero-detail",
-    templateUrl: "./hero-detail.component.html",
-    styleUrls: ["./hero-detail.compontent.css"]
+    templateUrl: "./hero-detail.component.html"
 })
 export class HeroDetailComponent{
     @Input() hero:Hero;
