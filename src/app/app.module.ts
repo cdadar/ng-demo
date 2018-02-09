d6d57d8bfb0a313b4b51886b740fca2ddf6f5c5d
cdadar <gunbanmi@gmail.com>
2018-02-09 15:52:06 +0800

    HTTP v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 @NgModule({
     declarations: [
@@ -22,11 +28,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+
+        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+        // and returns simulated server responses.
+        // Remove it when a real server is ready to receive requests.
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        )
     ],
     providers: [
         HeroService,
-        MessageService
+        MessageService,
+        InMemoryDataServiceService
     ],
     bootstrap: [ AppComponent ]
 })
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,40 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
+    private heroesUrl = 'api/heroes'; //URL to web api
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService) { }
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+        this.messageService.add('HeroService: ' + message);
+    }
 
     getHeroes(): Observable<Hero[]> {
         // Todo: send the message _after_ fetching the heroes
         this.messageService.add('HeroService: fetched heroes');
-        return of(HEROES);
+        return this.http.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                tap(heroes => this.log('fetched heroes')),
+                catchError(this.handleError('getHeroes',[]))
+            );
     }
 
     getHero(id: number): Observable<Hero> {
-        // Todo: send the message _after_ fetching the hero
-        this.messageService.add('HeroService: fetched hero id=${id}');
-        return of(HEROES.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get<Hero>(url).pipe(
+            tap(_ => this.log(`fetched hero id=${id}`)),
+            catchError(this.handleError<Hero>(`getHero id=${id}`))
+        );
     }
 }
