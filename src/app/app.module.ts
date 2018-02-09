ae4a6c379db5b444065820c802a336144e0d3e1d
cdadar <gunbanmi@gmail.com>
2018-02-09 15:04:56 +0800

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [MessageService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessagesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [
+        HeroService,
+        MessageService
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
